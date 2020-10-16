917193d05dcd13abf533918cc261845d46e724f2
Irfan <>
2020-10-16 19:36:33 +0700

    upload project

---
+++ src/pboif2/pkg10119056/latihan7/integer/PBOIF210119056Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119056.latihan7.integer;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Irfan Ginanjar
+ * KELAS    : PBOIF2
+ * NIM      : 10119056
+ * Deskripsi Program: Program ini berisi program untuk menampilkan tipe data intrger
+ */
+public class PBOIF210119056Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b=120;
+        s=32767;
+        i=1_000_000_000;
+        l=90000000000L;
+        System.out.println("byte : "+b);
+        System.out.println("short : "+s);
+        System.out.println("int : "+i);
+        System.out.println("long : "+l);
+    }
+    
+}
